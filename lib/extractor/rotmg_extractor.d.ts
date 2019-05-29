209d745f1e78ddd81aca506072233cd0f5173cda
thomas-crane <thomasgcrane@gmail.com>
2019-05-30 09:55:24 +1000

    feat(lib): add new wasm binary

---
+++ lib/extractor/rotmg_extractor.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/**
+* @param {Uint8Array} client 
+* @returns {any} 
+*/
+export function extract_mappings(client: Uint8Array): any;
---
+++ lib/extractor/rotmg_extractor_bg.d.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+export const memory: WebAssembly.Memory;
+export function extract_mappings(a: number, b: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_free(a: number, b: number): void;
